3c5a3e7ea9901264c1dbe9f2b005fb02df25c53e
jacob <jacob@jacobs-mbp.lan>
2021-11-29 13:59:46 -0600

    solution

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
-var output = document.querySelector('.output');
+"use strict";
+let output = document.querySelector('.output');
 /*declare the following variables and types
   num1 as number
   num2 as number
@@ -6,8 +7,9 @@ var output = document.querySelector('.output');
   
   Figure out how to set the values of num1 and num2 as the buttons are clicked
   */
-document.querySelector('.calc').addEventListener('click', function (evt) {
-    var button = evt.target;
+let num1, num2, operand;
+document.querySelector('.calc').addEventListener('click', (evt) => {
+    let button = evt.target;
     if (button.className.indexOf('num') !== -1) {
         console.log(button.innerHTML);
         output.value += button.innerHTML;
@@ -18,15 +20,43 @@ document.querySelector('.calc').addEventListener('click', function (evt) {
     }
     if (button.className.indexOf('equal') !== -1) {
         console.log(button.innerHTML);
-        var calculation = calculate( /*pass in params here*/);
+        const NumRegex = /\D/, OpRegex = /\d/;
+        let calculation = calculate(output.value.split(NumRegex), output.value.split(OpRegex).filter(trash => trash !== ''));
         output.value = calculation.toString();
+    }
+    if (button.className.indexOf('reset') !== -1) {
+        output.value = '';
     }
     /* create another condition to clear the value of the input when C is pressed*/
 });
 //create a function that takes in 2 numbers and a string (operand)
-function calculate( /*You fill in here*/) {
+function calculate(numArray, opArray) {
     /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
     return that value
     */
-    return 0;
+    let calc = +numArray[0];
+    console.log(`Op: ${opArray} | num: ${numArray}`);
+    for (let i = 0; i < opArray.length; i++) {
+        switch (opArray[i]) {
+            case '+':
+                calc += +numArray[i + 1];
+                break;
+            case '-':
+                calc -= +numArray[i + 1];
+                break;
+            case '*':
+                calc *= +numArray[i + 1];
+                break;
+            case '/':
+                if (+numArray[i + 1] === 0) {
+                    return "Err: can't divide by zero.";
+                }
+                calc /= +numArray[i + 1];
+                break;
+        }
+    }
+    //const numArray = output.value.split(NumRegex), opArray = output.value.split(OpRegex).filter(trash => trash !== '');
+    //    console.log(output.value.split(NumRegex));
+    //    console.log(output.value.split(OpRegex).filter(trash => trash !== ''));
+    return calc;
 }
--- calc.ts
+++ calc.ts
@@ -6,6 +6,8 @@ let output = document.querySelector('.output') as HTMLInputElement;
     
     Figure out how to set the values of num1 and num2 as the buttons are clicked
     */
+   let num1:number, num2:number, operand:string;
+
 document.querySelector('.calc').addEventListener('click',(evt)=>{
     let button = evt.target as HTMLButtonElement;
   
@@ -19,17 +21,48 @@ document.querySelector('.calc').addEventListener('click',(evt)=>{
     }
     if(button.className.indexOf('equal')!==-1){
         console.log(button.innerHTML);
-        let calculation = calculate(/*pass in params here*/);
+        const NumRegex:RegExp = /\D/, OpRegex:RegExp = /\d/;
+        let calculation = calculate(output.value.split(NumRegex), output.value.split(OpRegex).filter(trash => trash !== ''));
         output.value = calculation.toString();
     }
+    if(button.className.indexOf('reset')!==-1){
+       output.value = '';
+    }
+    
     /* create another condition to clear the value of the input when C is pressed*/
 });
 
 //create a function that takes in 2 numbers and a string (operand)
-function calculate(/*You fill in here*/){
+function calculate(numArray:string[], opArray:string[]){
     /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
     return that value
     */
-   return 0;
+   let calc = +numArray[0];
+   console.log(`Op: ${opArray} | num: ${numArray}`);
+   for(let i = 0; i < opArray.length; i++){
+       switch (opArray[i]){
+           case '+':
+               calc += +numArray[i+1];
+               break;
+            case '-':
+                calc -= +numArray[i+1];
+                break;
+            case '*':
+                calc *= +numArray[i+1];
+                break;
+            case '/':
+                if(+numArray[i+1] === 0){ return "Err: can't divide by zero."}
+                calc /= +numArray[i+1];
+                break;
+
+       }
+   }
+   //const numArray = output.value.split(NumRegex), opArray = output.value.split(OpRegex).filter(trash => trash !== '');
+
+
+//    console.log(output.value.split(NumRegex));
+//    console.log(output.value.split(OpRegex).filter(trash => trash !== ''));
+   
+   return calc;
 
 }
